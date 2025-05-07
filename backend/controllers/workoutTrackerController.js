b15eb80bed3ea1b7c039e48dc839c91a0b37aafe
shrutix <shrspam8@gmail.com>
2025-05-07 16:41:35 -0400

    sql struct

--- backend/controllers/workoutTrackerController.js
+++ backend/controllers/workoutTrackerController.js
@@ -1,3 +1,4 @@
+import workoutTrackerDB from "../models/workoutTrackerModel.js";
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
--- backend/models/workoutTrackerModel.js
+++ backend/models/workoutTrackerModel.js
@@ -1,46 +1,86 @@
-class _workoutTrackerModel {
-    static workoutid = 1;
+import { DataTypes, Model } from 'sequelize';
 
-    constructor() {
-        this.workouts = [];
-    }
-
-    async create(workout) {
-        workout.id = _InMemoryTaskModel.workoutid++;
-        this.workouts.push(workout);
-        return workout;
-    }
-
-    async read(id = null) {
-        if (id) {
-            return this.workouts.find((workout) => workout.id === id);
-        }
-
-        return this.workouts;
-    }
-
-    async update(workout) {
-        const index = this.workouts.findIndex((t) => t.id === workout.id);
-        this.workouts[index] = workout;
-        return wokrout;
-    }
-
-    async delete(workout = null) {
-        if (workout === null) {
-            this.workouts = [];
-            return;
-        }
-
-        const index = this.workouts.findIndex((t) => t.id === workout.id);
-        this.workouts.splice(index, 1);
-        return workout;
+class workoutTrackerDB extends Model {
+    static init(sequelize) {
+        super.init(
+            {
+                date: {
+                    type: DataTypes.DATE,
+                    allowNull: false,
+                },
+                workoutType: {
+                    type: DataTypes.STRING,
+                    allowNull: false,
+                },
+                duration: {
+                    type: DataTypes.INTEGER,
+                    allowNull: false,
+                },
+                distance: {
+                    type: DataTypes.FLOAT,
+                    allowNull: false,
+                },
+                rpe: {
+                    type: DataTypes.INTEGER,
+                    allowNull: false,
+                },
+                notes: {
+                    type: DataTypes.STRING,
+                    allowNull: true,
+                },
+            },
+            {
+                sequelize,
+                modelName: 'WorkoutTracker',
+            }
+        );
     }
 }
 
-const workoutTrackerModel = new _workoutTrackerModel();
+// class _workoutTrackerModel {
+//     static workoutid = 1;
 
-workoutTrackerModel.create({ date: "2023-10-01", workoutType: "Run", duration: 30, distance: 5, rpe: 7, notes: "Note" });
-workoutTrackerModel.create({ date: "2023-10-02", workoutType: "Swim", duration: 45, distance: 2, rpe: 8, notes: "Note" });
-workoutTrackerModel.create({ date: "2023-10-03", workoutType: "Bike", duration: 60, distance: 20, rpe: 6, notes: "Note" });
+//     constructor() {
+//         this.workouts = [];
+//     }
 
-export default workoutTrackerModel;
+//     async create(workout) {
+//         workout.id = _InMemoryTaskModel.workoutid++;
+//         this.workouts.push(workout);
+//         return workout;
+//     }
+
+//     async read(id = null) {
+//         if (id) {
+//             return this.workouts.find((workout) => workout.id === id);
+//         }
+
+//         return this.workouts;
+//     }
+
+//     async update(workout) {
+//         const index = this.workouts.findIndex((t) => t.id === workout.id);
+//         this.workouts[index] = workout;
+//         return wokrout;
+//     }
+
+//     async delete(workout = null) {
+//         if (workout === null) {
+//             this.workouts = [];
+//             return;
+//         }
+
+//         const index = this.workouts.findIndex((t) => t.id === workout.id);
+//         this.workouts.splice(index, 1);
+//         return workout;
+//     }
+// }
+
+// const workoutTrackerModel = new _workoutTrackerModel();
+
+// workoutTrackerModel.create({ date: "2023-10-01", workoutType: "Run", duration: 30, distance: 5, rpe: 7, notes: "Note" });
+// workoutTrackerModel.create({ date: "2023-10-02", workoutType: "Swim", duration: 45, distance: 2, rpe: 8, notes: "Note" });
+// workoutTrackerModel.create({ date: "2023-10-03", workoutType: "Bike", duration: 60, distance: 20, rpe: 6, notes: "Note" });
+
+// export default workoutTrackerModel;
+export default workoutTrackerDB;
